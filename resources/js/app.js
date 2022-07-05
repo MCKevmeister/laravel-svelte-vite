a7bbc323df17f49bfd4f13f95670cbf11761f8ef
Mark <markchristison@gmail.com>
2022-07-06 10:45:32 +1200

    Set up client side

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createInertiaApp } from '@inertiajs/inertia-svelte'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css';
+
+createInertiaApp({
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob('./Pages/**/*.svelte')),
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+})
