b0919b1025b14a1656342be8d3c728461c504763
Mark <markchristison@gmail.com>
2022-07-06 10:54:19 +1200

    Configure Vite and TailwindCSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from "path";
 
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        svelte(),
+
     ],
+    optimizeDeps: {
+        include: ['@inertiajs/inertia'],
+    },
+    resolve: {
+        alias: {
+            '@': resolve('./resources/js'),
+        },
+    },
 });
